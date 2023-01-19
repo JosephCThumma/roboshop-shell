347f677af9fff02a09dae0c0ba5f99a42e311081
Joseph <joseph.cthumma@gmail.com>
2023-01-19 12:31:58 -0500

    roboshop-catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -13,7 +13,7 @@ unzip /tmp/catalogue.zip
 cd /app
 npm install
 
-cp ${script_location} /files\catlogue.service /etc/systemd/system/catalogue.service
+cp ${script_location}/files\catlogue.service /etc/systemd/system/catalogue.service
 
 systemctl daemon-reload
 systemctl enable catalogue
